daf5185acf98388a04da7e15accb526bb62ee7dc
wilsonv244 <39635463+wilsonv244@users.noreply.github.com>
2021-09-30 23:08:24 -0500

    probando js

---
+++ js/Wjs.js
@@ -0,0 +1,31 @@
+const campana = document.querySelector('#campanita');
+const noti = document.querySelector('#boton');
+const corazon= document.querySelector('#corazon');
+// const estilo = CSS.querySelector('.alerta');
+
+campana.addEventListener('click', function () {
+    Notification.requestPermission()
+    .then(result => console.log(`se activo la notificacion ${result}`));
+    if(campana == 'granted'){
+        campana.style.color='yellow';
+    }
+    
+})
+corazon.addEventListener('click',function () {
+    if (Notification.permission == 'granted') {
+        new Notification('gracias por estar aqui',{
+            icon: 'img/img_wilson/LOGO2.png',
+            body: 'gracias por darle me encorazona'
+        });
+        // campana.style.color='yellow';
+    }else{
+        campana.style.color= 'orange';
+    }
+})
+
+if (Notification.permission=='denied' || Notification.permission=='default') {
+    campana.style.color='red';
+}
+if(Notification.permission == 'granted'){
+    campana.style.color='yellow';
+}
